9056702968fd520fedb41657f8c1b92055245564
yonicd <yonicd@gmail.com>
2017-02-11 22:39:42 -0500

    init commit

---
+++ R/slickR.R
@@ -0,0 +1,51 @@
+#' <Add Title>
+#'
+#' <Add Description>
+#'
+#' @import htmlwidgets
+#'
+#' @export
+slickR <- function(style, ... , width = NULL, height = NULL, elementId = NULL) {
+
+  # forward options using x
+  x = list(...)
+  x$style=style
+
+  # create widget
+  htmlwidgets::createWidget(
+    name = 'slickR',
+    x,
+    width = width,
+    height = height,
+    package = 'slickR',
+    elementId = elementId
+  )
+}
+
+#' Shiny bindings for slickR
+#'
+#' Output and render functions for using slickR within Shiny
+#' applications and interactive Rmd documents.
+#'
+#' @param outputId output variable to read from
+#' @param width,height Must be a valid CSS unit (like \code{'100\%'},
+#'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
+#'   string and have \code{'px'} appended.
+#' @param expr An expression that generates a slickR
+#' @param env The environment in which to evaluate \code{expr}.
+#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
+#'   is useful if you want to save an expression in a variable.
+#'
+#' @name slickR-shiny
+#'
+#' @export
+slickROutput <- function(outputId, width = '100%', height = '400px'){
+  htmlwidgets::shinyWidgetOutput(outputId, 'slickR', width, height, package = 'slickR')
+}
+
+#' @rdname slickR-shiny
+#' @export
+renderSlickR <- function(expr, env = parent.frame(), quoted = FALSE) {
+  if (!quoted) { expr <- substitute(expr) } # force quoted
+  htmlwidgets::shinyRenderWidget(expr, slickROutput, env, quoted = TRUE)
+}
